f4429e8baf4b32c8fb6550e292d1a59a680820a1
Ademario Ramos <ramoscodev@gmail.com>
2023-10-06 16:33:53 -0300

    Segunda aula- Declaração de Variaveis

--- Aula01/MyFirstApp/src/App.java
+++ Aula01/MyFirstApp/src/App.java
@@ -1,5 +1,29 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Java World!");
+        //System.out.println("Hello, Java World!");
+        // Segunda implementação de código
+        // declarando variaveis
+        String titulo;
+        String nomePessoa;
+        int idade;
+        float salario;
+        boolean ativo;
+
+        // Usando as variáveis declaradas acima.
+        titulo = "Segunda aula - Mão no código";
+
+        nomePessoa = "Ademario Ramos";
+
+        idade = 44;
+
+        salario = 3000.55F;
+
+        ativo = true;
+
+        System.out.println("Título: " + titulo);
+        System.out.println("Nome da Pessoa: " + nomePessoa);
+        System.out.println("Idade: " + idade);
+        System.out.println("Salário: " + salario);
+        System.out.println("Ativo: " + ativo);
     }
 }
